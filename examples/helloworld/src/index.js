747c8304ba6b75563450892e13dbc7ce4315e0dd
Shuichiro Kamiya <notsunohito@gmail.com>
2017-03-12 15:20:37 +0900

    Add examples/helloworld

---
+++ examples/helloworld/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {render} from 'react-dom'
+import {createStore, Provider} from 'orex'
+
+
+const initialState = { message: 'Hello World!' }
+const store = createStore(initialState)
+
+const HelloWorld = ({state, action})=> (
+    <div>
+        <input
+            type="text"
+            value={state.message}
+            onChange={(e)=> action.message.update(e.target.value)}
+        />
+        <p>{state.message}</p>
+    </div>
+)
+
+render(
+    <Provider store={store}>
+        <HelloWorld />
+    </Provider>,
+    document.querySelector('#app')
+)
