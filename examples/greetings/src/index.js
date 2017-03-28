3e2aec8eaa52e6b758903ecb126ceaf7f1f1e70d
Shuichiro Kamiya <notsunohito@gmail.com>
2017-03-28 23:43:29 +0900

    Change examples indent 4 spaces to 2 spaces

--- examples/greetings/src/index.js
+++ examples/greetings/src/index.js
@@ -7,17 +7,17 @@ const initialState = { message: 'Greetings' }
 const store = createStore(initialState)
 
 const Greeting = ({state, action})=> (
-    <div>
-        <h1>{state.message}</h1>
-        <button onClick={()=> action.message.update('Hello.')}>en</button>
-        <button onClick={()=> action.message.update('你好。')}>zh</button>
-        <button onClick={()=> action.message.update('こんにちは。')}>ja</button>
-    </div>
+  <div>
+    <h1>{state.message}</h1>
+    <button onClick={()=> action.message.update('Hello.')}>en</button>
+    <button onClick={()=> action.message.update('你好。')}>zh</button>
+    <button onClick={()=> action.message.update('こんにちは。')}>ja</button>
+  </div>
 )
 
 render(
-    <Provider store={store}>
-        <Greeting />
-    </Provider>,
-    document.querySelector('#app')
+  <Provider store={store}>
+    <Greeting />
+  </Provider>,
+  document.querySelector('#app')
 )
--- examples/helloworld/src/index.js
+++ examples/helloworld/src/index.js
@@ -7,19 +7,19 @@ const initialState = { message: 'Hello World!' }
 const store = createStore(initialState)
 
 const HelloWorld = ({state, action})=> (
-    <div>
-        <input
-            type="text"
-            value={state.message}
-            onChange={(e)=> action.message.update(e.target.value)}
-        />
-        <p>{state.message}</p>
-    </div>
+  <div>
+    <input
+      type="text"
+      value={state.message}
+      onChange={(e)=> action.message.update(e.target.value)}
+    />
+    <p>{state.message}</p>
+  </div>
 )
 
 render(
-    <Provider store={store}>
-        <HelloWorld />
-    </Provider>,
-    document.querySelector('#app')
+  <Provider store={store}>
+    <HelloWorld />
+  </Provider>,
+  document.querySelector('#app')
 )
