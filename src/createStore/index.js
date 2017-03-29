95516bf0edebd5d16545ade68ba22f8dfc2efeb2
Shuichiro Kamiya <notsunohito@gmail.com>
2017-03-30 00:21:36 +0900

    Don't let getAction recreate ActionProxy

--- src/createStore/index.js
+++ src/createStore/index.js
@@ -4,13 +4,14 @@ import createActionProxy from './createActionProxy'
 
 export default function createStore(initialState:Object) {
     const store = new Store(initialState)
+    const action = createActionProxy(store)
 
     function getState() {
         return store.getState()
     }
 
     function getAction() {
-        return createActionProxy(store)
+        return action
     }
 
     function subscribe(fn:Function) {
